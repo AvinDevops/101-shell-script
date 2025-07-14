9448686a77d89aea64b06709141173bba1663eac
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-14 21:54:42 +0530

    using functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -12,12 +12,19 @@ fi
 
 VALIDATE(){
 
-    echo "Exit status: $1"
-    echo "What are you doing: $2"
+    # echo "Exit status: $1"
+    # echo "What are you doing: $2"
 
+    if [ $1 -ne 0 ]
+    then
+        echo "$2... is FAILED"
+        exit
+    else
+        echo "$2... is SUCCESS"
+    fi
 }
 
-dnf install mysql -y
+dnf install mysqll -y
 VALIDATE $? "Installing mysql"
 
 
