58682302f28b380e51761f737a0596772f381efb
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-14 22:50:17 +0530

    using loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -4,7 +4,53 @@
 # do 
 #     echo $i 
 # done
+# for i in $@
+# do 
+#     echo $i 
+# done
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+#getting colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[32m"
+N="\e[0m"
+
+#creating function
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....is $R FAILED $N"
+        exit
+    else   
+        echo -e "$2....is $G SUCCESS $N"
+    fi
+}
+
+#checking root user or not
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R you are not root user $N"
+    exit
+else
+    echo -e "$G you are root user $N"
+fi
+
 for i in $@
-do 
-    echo $i 
+do
+    echo "Installing package is: $i"
+    dnf list installed $i &>>LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$G $i is already installed $N"
+        exit
+    else
+        echo "$R $i is not installed $N"
+        dnf install $i -y &>>LOGFILE
+        VALIDATE $? "Installing $i"
+    fi
 done
