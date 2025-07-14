cd9691b3f7dfc59a21932506aa9dcb176c88159a
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-14 19:54:11 +0530

    with arguments

---
+++ 04-with-arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring variables
+PERSON1=$1
+PERSON2=$2
+
+#refering variables
+echo "$PERSON1: Hello $PERSON2, how are you doing?"
+echo "$PERSON2: Hi $PERSON1, I am fine. How about you?"
+echo "$PERSON1: I am doing good, how is your work!"
+echo "$PERSON2: not bad. I am thinking to upgrade to devops."
