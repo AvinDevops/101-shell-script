0e91649bc65ef4f7e2f4f886c7b4a0f55edb707c
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-14 20:09:23 +0530

    using arrays

--- 06-using-array.sh
+++ 06-using-array.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-MOVIES=("Avengers","Infinity war","End game")
+MOVIES=("Avengers" "Infinity war" "End game")
 
 #for this array range is 3
 #array starts with index 0
 
-echo "first movie is: $MOVIES[0]"
+echo "first movie is: ${MOVIES[0]}"
+echo "second movie is: ${MOVIES[1]}"
+echo "All movies are: ${MOVIES[@]}"
