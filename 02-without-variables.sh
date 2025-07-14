51e1b87d1adeae3a0784b80bbc43eece8acf4fa9
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-14 19:43:08 +0530

    with out variables

---
+++ 02-without-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Suresh, how are you doing?"
+echo "Suresh: Hi Ramesh, I am fine. How about you?"
+echo "Ramesh: I am doing good, how is your work!"
+echo "Suresh: not bad. I am thinking to upgrade to devops."
