364b2c0c8003a6bf4d67d0021e4e7d5172b75e79
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-14 21:28:14 +0530

    installing apps

--- 10-installing-apps.sh
+++ 10-installing-apps.sh
@@ -5,12 +5,29 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "you are not root user, please run run with root access"
+    exit 1
 else
     echo "you are root user"
 fi
 
 dnf install mysql -y
+if [ $? -ne 0 ]
+then 
+    echo "Installation is failed"
+    exit 0
+else
+    echo "Installation is success"
+fi
+
 
 dnf install git -y
+if [ $? -ne 0 ]
+then 
+    echo "Installation is failed"
+    exit
+else
+    echo "Installation is success"
+fi
+
 
 echo "Execution reached last line of code"
