1dd008a51ae0e568743a4677545451282c371416
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-16 22:56:15 +0530

    disk alert

---
+++ 18-disk-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_SPACE=$(df -hT | grep xfs)
+DISK_THRESHOLD=10
+
+while IFS= read -r line
+do
+    USAGE=$($line | awk -F " " '{print $6F}')
+    FOLDER=$($line | awk -F " " '{print $NF}')
+
+    if [ $USAGE -gt $DISK_THRESHOLD ]
+    then
+        echo "This folder: $FOLDER is more than $DISK_THRESHOLD, current usage is: $USAGE"
+    fi
+
+done <<< $DISK_SPACE
