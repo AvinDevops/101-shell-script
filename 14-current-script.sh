e4168f21290a964d65919a612ada2e097ccd9401
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-16 21:40:21 +0530

    calling other script in current script

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -5,6 +5,6 @@ COURSE="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "PID of the current shell script: $$"
 
-#./15-other-script.sh 
+./15-other-script.sh 
 
 echo "After calling other script, course: $COURSE"
