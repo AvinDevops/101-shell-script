bfde40896a1373f75e37dd4765522b1e71a62880
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 17:52:10 +0530

    installing packages

--- 15-installing-packages.sh
+++ 15-installing-packages.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 USER=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USER -ne 0 ]
 then
-    echo "you are not root user, please access with root user"
+    echo -e "$R you are not root user, please access with root user $N"
     exit
 else
-    echo "you are root user"
+    echo -e "$G you are root user $N"
 fi
 
 for i in $@
 do
     echo $i
-    dnf list installed $i
+    dnf list installed $i &>>$LOGFILE
     if [ $? -eq 0 ]
     then
-        echo "$i is installed...SKIPPING"
+        echo -e "$i is installed...$Y SKIPPING $N"
     else
-        echo "$i is not installed"
+        echo -e "$i is not $R installed $N"
     fi
 done
