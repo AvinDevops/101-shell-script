69bd41e988ab4e8d38738a2cac66f9500d362ccb
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 13:27:12 +0530

    installing package

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,7 +10,21 @@ else
 fi
 
 dnf install mysqll -y
+if [ $? -ne 0 ]
+then
+    echo "Installing mysql is...FAILED"
+    exit
+else
+    echo "Installing mysql is...SUCCESS"
+fi
 
 dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "Installing git is...FAILED"
+    exit
+else
+    echo "Installing git is...SUCCESS"
+fi
 
 echo "is script reached end..!"
