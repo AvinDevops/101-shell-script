7ce72848418aa405faa188950440c63b3b1c7781
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 13:02:56 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NUMBER=10
+NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
 then
-    echo "given $NUMBER is greater than 10"
+    echo "given num:$NUMBER is greater than 10"
 else
-    echo "given $NUMBER is less than 10"
+    echo "given num:$NUMBER is less than 10"
 fi
