1bf8ad99bfa9e31fbcb96b4a4feabe376998039f
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 12:25:06 +0530

    prompt variable

---
+++ 05-prompt-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username:"
+
+read USERNAME
+
+echo "please enter password:"
+
+read PASSWORD
+
+echo "Your username is: $USERNAME and password is: $PASSWORD"
