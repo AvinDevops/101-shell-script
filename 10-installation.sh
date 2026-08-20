777a86dfad2963f79507e43d062d16c80309ec7e
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 13:11:41 +0530

    installing package

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+USER=$(id -u)
+if [ $USER -ne 0 ]
+then
+    echo "you are not root user, please access with root access"
+else
+    echo "you are root user"
+fi
+
 dnf install mysql -y
