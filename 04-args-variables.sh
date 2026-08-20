f553585d4f39770ef401d500a3458180c575305a
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 12:20:29 +0530

    passing args to variables

---
+++ 04-args-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, how are you doing!"
+echo "$PERSON2: Hi $PERSON1, I am good."
+echo "$PERSON1: How is your work going on!"
+echo "$PERSON2: Good, and I am learning DevOps"
