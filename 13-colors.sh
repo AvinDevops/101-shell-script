adb92bd403e6c26e84599f1cc916c0dfc8335faa
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 17:23:01 +0530

    colors

--- 13-colors.sh
+++ 13-colors.sh
@@ -5,21 +5,27 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
+#colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $USER -ne 0 ]
 then
-    echo "you are not root user, please access with root"
+    echo -e "$R you are not root user, please access with root $N"
     exit
 else
-    echo "you are root user"
+    echo -e "$G you are root user $N"
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is...FAILED"
+        echo -e "$2 is...$R FAILED $N"
         exit
     else
-        echo "$2 is...SUCCESS"
+        echo -e "$2 is...$G SUCCESS $Y"
     fi
 }
 
