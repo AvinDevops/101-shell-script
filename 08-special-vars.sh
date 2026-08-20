1d477bd4c7bafa8e661b43bc8b3a8065da3057ec
AvinDevops <avinashbandela73@gmail.com>
2026-08-20 12:52:15 +0530

    special variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,7 @@
 echo "All variables passed: $@"
 echo "Num of variables passed: $#"
 echo "Current shell script name: $0"
+echo "Current username: $USER"
+echo "Current working dir: $PWD"
+echo "Current hostname: $HOSTNAME"
+echo "process id of current shell script: $$"
