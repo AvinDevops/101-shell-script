0df602e7a891b5c0424ee5adcf97f607fd57a25d
AvinDevops <avinashbandela73@gmail.com>
2026-08-28 00:06:01 +0530

    using set

--- 18-set.sh
+++ 18-set.sh
@@ -7,10 +7,10 @@ N="\e[0m"
 USER=$(id -u)
 if [ $USER -ne 0 ]
 then
-    echo "you are not super user, please access with super user"
+    echo -e "$R you are not super user, please access with super user $N"
     exit 1
 else
-    echo "you are super user"
+    echo -e "$R you are super user $N"
 fi
 
 dnf install mysqllll -y
