be16a5f67bcc54d92b8025e7fbcf2fc1d24db649
AvinDevops <avinashbandela73@gmail.com>
2026-08-27 23:41:21 +0530

    script-1

--- 16-script-1.sh
+++ 16-script-1.sh
@@ -5,6 +5,7 @@ COURSE="DevOps With AWS"
 echo "Before calling script-2: $COURSE"
 echo "Process ID of script-1: $$"
 
-./17-script-2.sh
+# ./17-script-2.sh
+source ./17-script-2.sh
 
 echo "After calling script-2: $COURSE"
