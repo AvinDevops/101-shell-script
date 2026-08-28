a618e1e0666f587b49fb1c9a302d8a53a81ed267
AvinDevops <avinashbandela73@gmail.com>
2026-08-29 00:30:42 +0530

    disk useage

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -12,6 +12,7 @@ do
     # echo "useage is:$USEAGE"
     if [ $USEAGE -ge $DISK_THRESHOLD ]
     then
-        echo "Folder:$FOLDER is more than $DISK_THRESHOLD, current useage is: $USEAGE"
+        # echo "Folder:$FOLDER is more than $DISK_THRESHOLD, current useage is: $USEAGE"
+        MESSAGE="Folder:$FOLDER is more than $DISK_THRESHOLD, current useage is: $USEAGE"
     fi
 done <<<$DISK_USEAGE
