3d9661e4ee03ebb61c1d852d98f5c29c88607b05
AvinDevops <avinashbandela73@gmail.com>
2026-08-29 00:28:34 +0530

    disk useage

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -3,13 +3,13 @@
 DISK_USEAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
 
-echo "disk_useage is: $DISK_USEAGE"
+# echo "disk_useage is: $DISK_USEAGE"
 
 while IFS= read -r line
 do 
     USEAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    echo "useage is:$USEAGE"
+    # echo "useage is:$USEAGE"
     if [ $USEAGE -ge $DISK_THRESHOLD ]
     then
         echo "Folder:$FOLDER is more than $DISK_THRESHOLD, current useage is: $USEAGE"
